b53c5f0039309efad0798c10e5416e00ab2d307a
CharlyHey <89465707+CharlyHey@users.noreply.github.com>
2021-09-28 23:52:30 -0500

    Update widget.dart

--- widget.dart
+++ widget.dart
@@ -1,1 +1,224 @@
-Widget de imagen
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Pad thai”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/PadThai.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Kuay Teow Lui Suan”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/KuayTeow.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Tom Yum Goong”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/TomYum.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Tom Kha Kai”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/TomKha.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Kai Med Ma Muang”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/KaiMed.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Massaman Curry”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Massam.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Som tum”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/SomTum.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Khao niao Mamuang”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Khao.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Kluay Buat Chi”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Kluay.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Durian Ice Cream”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Durian.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Sanay chan”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Sanay.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Khanom Chan”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Khanom.jpg”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Nam Maphrao”),
+                     ),
+                     body: Column(
+                          children: [Nam.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+Import ‘package:flutter/material.dart’;
+Class MyImageWidget extends StatelessWidget {
+       @override
+          Widget build(BuildContext context) {
+               Return Scaffold(
+                     appBar: AppBar(
+                            title: Text(“Cha Yen”),
+                     ),
+                     body: Column(
+                          children: [
+                               Image.asset(“assets/images/Cha.png”),
+                           ],
+                        ),
+                     );
+                  }
+              }
+
